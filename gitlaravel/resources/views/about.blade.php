019390e7945dc4986de221592444c7b935a5c908
tugz <jayryan@myvirtual.services>
2019-01-17 16:02:07 +0800

    add two blade files

---
+++ gitlaravel/resources/views/about.blade.php
@@ -0,0 +1,41 @@
+@extends('theme.default')
+
+
+
+@section('content')
+<div class="row">
+
+    
+        <div class="input-group custom-search-form">
+
+                    <input type="text" class="form-control" placeholder="Search...">
+
+                    <span class="input-group-btn">
+
+                    <button class="btn btn-default" type="button">
+
+                        <i class="fa fa-search"></i>
+
+                    </button>
+
+                </span>
+
+                </div>
+
+    
+
+                
+
+    <div class="row topbox">
+        <div class="col-lg-12">
+            <p class="page-header">About</p>
+        </div>
+         
+
+    </div>       
+    
+
+</div>
+
+<!-- /.row -->
+@endsection
