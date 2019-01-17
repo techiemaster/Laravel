694a5b367781f490085f58a52ab9df45f71ce849
tugz <jayryan@myvirtual.services>
2019-01-17 15:04:01 +0800

    commit gitlaravel files

---
+++ gitlaravel/resources/views/theme/sidebar.blade.php
@@ -0,0 +1,101 @@
+<div class="navbar-default sidebar" role="navigation">
+
+    <div class="sidebar-nav navbar-collapse">
+
+        <ul class="nav" id="side-menu">
+
+            
+
+            <li>
+
+                <a href="index.html"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+
+            </li>
+
+            <li>
+
+                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+
+                <ul class="nav nav-second-level">
+
+                    <li>
+
+                        <a href="flot.html">Flot Charts</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="morris.html">Morris.js Charts</a>
+
+                    </li>
+
+                </ul>
+
+                <!-- /.nav-second-level -->
+
+            </li>
+
+            <li>
+
+                <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+
+            </li>
+
+            <li>
+
+                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+
+            </li>
+
+            <li>
+
+                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+
+                <ul class="nav nav-second-level">
+
+                    <li>
+
+                        <a href="panels-wells.html">Panels and Wells</a>
+
+                    </li>
+
+                </ul>
+
+                <!-- /.nav-second-level -->
+
+            </li>
+
+            <li>
+
+                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+
+                <ul class="nav nav-second-level">
+
+                    <li>
+
+                        <a href="blank.html">Blank Page</a>
+
+                    </li>
+
+                    <li>
+
+                        <a href="login.html">Login Page</a>
+
+                    </li>
+
+                </ul>
+
+                <!-- /.nav-second-level -->
+
+            </li>
+
+        </ul>
+
+    </div>
+
+    <!-- /.sidebar-collapse -->
+
+</div>
+
+<!-- /.navbar-static-side -->
